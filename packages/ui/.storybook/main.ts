2082f90231f31e42ef6465fab2a74abc467c8a87
andrew9488 <pashkevichandrew@gmail.com>
2025-11-06 11:26:44 +0300

    dev-1633 install storybook and vite

---
+++ packages/ui/.storybook/main.ts
@@ -0,0 +1,25 @@
+import svgr from "vite-plugin-svgr";
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  async viteFinal(config) {
+    // Ensure the plugin is present in Storybook's Vite pipeline
+    config.plugins = [...(config.plugins ?? []), svgr()];
+    return config;
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+
+export default config;
