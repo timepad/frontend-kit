da3a4022ee8ef00ce0caa622d40db2c416ff284b
petrzhirkov <zhirkovpetr95@gmail.com>
2025-12-08 18:45:46 +0300

    DEV-1711 внес изменения вне папки packages/ui

--- packages/hooks/src/useAuth.ts
+++ packages/hooks/src/useAuth.ts
@@ -2,6 +2,8 @@ import { useState, useCallback } from 'react';
 
 export const useAuth = () => {
   console.log('useAuth hook called');
+  console.log('useAuth hook called11');
+  console.log('useAuth hook called22');
 
   return { user: 'user' };
 };
