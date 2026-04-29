0a3a24612ccafa22ff2b8c95d981c138fd406768
petrzhirkov <zhirkovpetr95@gmail.com>
2026-04-29 16:21:29 +0300

    DEV-1998 Добавил новые иконки arrows и objects

---
+++ packages/ui/src/assets/iconsNew/index.ts
@@ -0,0 +1,112 @@
+// arrows
+import IconArrowDown16Outline from "./arrows/ArrowDown-16-Outline.svg?react";
+import IconArrowDown24Outline from "./arrows/ArrowDown-24-Outline.svg?react";
+import IconArrowDown32Outline from "./arrows/ArrowDown-32-Outline.svg?react";
+import IconArrowLeft16Outline from "./arrows/ArrowLeft-16-Outline.svg?react";
+import IconArrowLeft24Outline from "./arrows/ArrowLeft-24-Outline.svg?react";
+import IconArrowLeft32Outline from "./arrows/ArrowLeft-32-Outline.svg?react";
+import IconArrowRight16Outline from "./arrows/ArrowRight-16-Outline.svg?react";
+import IconArrowRight24Outline from "./arrows/ArrowRight-24-Outline.svg?react";
+import IconArrowRight32Outline from "./arrows/ArrowRight-32-Outline.svg?react";
+import IconArrowUp16Outline from "./arrows/ArrowUp-16-Outline.svg?react";
+import IconArrowUp24Outline from "./arrows/ArrowUp-24-Outline.svg?react";
+import IconArrowUp32Outline from "./arrows/ArrowUp-32-Outline.svg?react";
+import IconChevronDown16Outline from "./arrows/ChevronDown-16-Outline.svg?react";
+import IconChevronDown24Outline from "./arrows/ChevronDown-24-Outline.svg?react";
+import IconChevronDown32Outline from "./arrows/ChevronDown-32-Outline.svg?react";
+import IconChevronLeft16Outline from "./arrows/ChevronLeft-16-Outline.svg?react";
+import IconChevronLeft24Outline from "./arrows/ChevronLeft-24-Outline.svg?react";
+import IconChevronLeft32Outline from "./arrows/ChevronLeft-32-Outline.svg?react";
+import IconChevronRight16Outline from "./arrows/ChevronRight-16-Outline.svg?react";
+import IconChevronRight24Outline from "./arrows/ChevronRight-24-Outline.svg?react";
+import IconChevronRight32Outline from "./arrows/ChevronRight-32-Outline.svg?react";
+import IconChevronUp16Outline from "./arrows/ChevronUp-16-Outline.svg?react";
+import IconChevronUp24Outline from "./arrows/ChevronUp-24-Outline.svg?react";
+import IconChevronUp32Outline from "./arrows/ChevronUp-32-Outline.svg?react";
+import IconDecrease16Outline from "./arrows/Decrease-16-Outline.svg?react";
+import IconDecrease24Outline from "./arrows/Decrease-24-Outline.svg?react";
+import IconDecrease32Outline from "./arrows/Decrease-32-Outline.svg?react";
+import IconIncrease16Outline from "./arrows/Increase-16-Outline.svg?react";
+import IconIncrease24Outline from "./arrows/Increase-24-Outline.svg?react";
+import IconIncrease32Outline from "./arrows/Increase-32-Outline.svg?react";
+import IconRedo16Outline from "./arrows/Redo-16-Outline.svg?react";
+import IconRedo24Outline from "./arrows/Redo-24-Outline.svg?react";
+import IconRedo32Outline from "./arrows/Redo-32-Outline.svg?react";
+import IconRefresh16Outline from "./arrows/Refresh-16-Outline.svg?react";
+import IconRefresh24Outline from "./arrows/Refresh-24-Outline.svg?react";
+import IconRefresh32Outline from "./arrows/Refresh-32-Outline.svg?react";
+import IconSort16Outline from "./arrows/Sort-16-Outline.svg?react";
+import IconSort24Outline from "./arrows/Sort-24-Outline.svg?react";
+import IconSort32Outline from "./arrows/Sort-32-Outline.svg?react";
+import IconSwap16Outline from "./arrows/Swap-16-Outline.svg?react";
+import IconSwap24Outline from "./arrows/Swap-24-Outline.svg?react";
+import IconSwap32Outline from "./arrows/Swap-32-Outline.svg?react";
+import IconUndo16Outline from "./arrows/Undo-16-Outline.svg?react";
+import IconUndo24Outline from "./arrows/Undo-24-Outline.svg?react";
+import IconUndo32Outline from "./arrows/Undo-32-Outline.svg?react";
+
+//objects
+import IconAddUser16Outline from "./objects/AddUser-16-Outline.svg?react";
+import IconAddUser24Outline from "./objects/AddUser-24-Outline.svg?react";
+import IconAddUser32Outline from "./objects/AddUser-32-Outline.svg?react";
+import IconUser16Outline from "./objects/User-16-Outline.svg?react";
+import IconUser24Outline from "./objects/User-24-Outline.svg?react";
+import IconUser32Outline from "./objects/User-32-Outline.svg?react";
+
+
+export {
+  // arrows
+  IconArrowDown16Outline,
+  IconArrowDown24Outline,
+  IconArrowDown32Outline,
+  IconArrowLeft16Outline,
+  IconArrowLeft24Outline,
+  IconArrowLeft32Outline,
+  IconArrowRight16Outline,
+  IconArrowRight24Outline,
+  IconArrowRight32Outline,
+  IconArrowUp16Outline,
+  IconArrowUp24Outline,
+  IconArrowUp32Outline,
+  IconChevronDown16Outline,
+  IconChevronDown24Outline,
+  IconChevronDown32Outline,
+  IconChevronLeft16Outline,
+  IconChevronLeft24Outline,
+  IconChevronLeft32Outline,
+  IconChevronRight16Outline,
+  IconChevronRight24Outline,
+  IconChevronRight32Outline,
+  IconChevronUp16Outline,
+  IconChevronUp24Outline,
+  IconChevronUp32Outline,
+  IconDecrease16Outline,
+  IconDecrease24Outline,
+  IconDecrease32Outline,
+  IconIncrease16Outline,
+  IconIncrease24Outline,
+  IconIncrease32Outline,
+  IconRedo16Outline,
+  IconRedo24Outline,
+  IconRedo32Outline,
+  IconRefresh16Outline,
+  IconRefresh24Outline,
+  IconRefresh32Outline,
+  IconSort16Outline,
+  IconSort24Outline,
+  IconSort32Outline,
+  IconSwap16Outline,
+  IconSwap24Outline,
+  IconSwap32Outline,
+  IconUndo16Outline,
+  IconUndo24Outline,
+  IconUndo32Outline,
+
+  //objects
+  IconAddUser16Outline,
+  IconAddUser24Outline,
+  IconAddUser32Outline,
+  IconUser16Outline,
+  IconUser24Outline,
+  IconUser32Outline,
+};
