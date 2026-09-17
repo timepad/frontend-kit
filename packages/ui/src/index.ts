e275040d62a701d891e3ccc3a8b90cb4db31cd5a
andrew9488 <pashkevichandrew@gmail.com>
2026-09-17 12:12:27 +0300

    dev-2065 правки селекторов

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,47 +2,36 @@
 
 import "./styles";
 
-export { Badge } from "./components/Badge";
-export { Counter } from "./components/Counter";
-export { Tabbar } from "./components/Tabbar";
-export { Breadcrumbs } from "./components/Breadcrumbs";
+export * from "./assets/icons";
+
 export { Avatar, AvatarStatus } from "./components/Avatar";
+export { Badge } from "./components/Badge";
+export { Breadcrumbs } from "./components/Breadcrumbs";
 export { Button } from "./components/Button";
+export { Cell } from "./components/Cell";
+export { Counter } from "./components/Counter";
+export { Divider } from "./components/Divider";
 export { IconButton } from "./components/IconButton";
-export { Input, PhoneInput, PriceInput, TimeInput, DiscountInput } from "./components/Input";
-export { Radio } from "./components/Radio";
-export { Switch } from "./components/Switch";
-export { Textarea } from "./components/Textarea";
-export { Typography } from "./components/Typography";
-export { Modal } from "./components/Modal";
-export type {
-  IModalProps,
-  ModalSize,
-  ModalHeaderAlign,
-  ModalFooterDirection,
-  ModalFooterLayout,
-  ButtonClickHandler,
-  IBtnCloseProps,
-  IBtnBackProps,
-} from "./components/Modal";
 export {
-  LayerProvider,
+  DiscountInput,
+  Input,
+  PhoneInput,
+  PriceInput,
+  TimeInput,
+} from "./components/Input";
+export {
+  getLayerPortalContainer,
   LayerHost,
+  LayerProvider,
   useLayer,
   useLayerHost,
-  getLayerPortalContainer,
 } from "./primitives/Layer";
-export type {
-  LayerDismissReason,
-  LayerOpenChangeDetails,
-  LayerOpenChangeHandler,
-  LayerDismissHandler,
-  IUseLayerOptions,
-  IUseLayerResult,
-} from "./primitives/Layer";
-export { Divider } from "./components/Divider";
-export { Snackbar } from "./components/Snackbar";
-export { Cell } from "./components/Cell";
 export { Link } from "./components/Link";
+export { Modal } from "./components/Modal";
+export { Radio } from "./components/Radio";
+export { Snackbar } from "./components/Snackbar";
+export { Switch } from "./components/Switch";
+export { Tabbar } from "./components/Tabbar";
 export { TabsButton } from "./components/TabsButton";
-export * from "./assets/icons";
+export { Textarea } from "./components/Textarea";
+export { Typography } from "./components/Typography";
